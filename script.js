663706e53e45fb6a02d6aa3471ff45f5e9b55018
AhoaT <ahoa.tran@gmail.com>
2022-01-29 23:56:08 +0000

    added game board to z-index

--- script.js
+++ script.js
@@ -5,7 +5,12 @@ btn.onclick = function show_hide() {
     let rules = document.querySelector('#rules');
     if (rules.style.display !== 'none') {
         rules.style.display = 'none';
+        btn.innerText = "Rules";
+
     } else {
         rules.style.display = 'block';
+        btn.innerText = "Close";
+        
     }
+    
 };
