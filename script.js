134e8e76017c148258446c8eacc1662c523678a9
AhoaT <ahoa.tran@gmail.com>
2022-02-07 14:38:08 +0000

    created for loop for board

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 const btn = document.querySelector('#rules-btn');
 
+//open and close rules when button is clicked
 
-btn.onclick = function show_hide() {
+function show_hide() {
     let rules = document.querySelector('#rules');
     if (rules.style.display !== 'none') {
         rules.style.display = 'none';
@@ -9,8 +10,23 @@ btn.onclick = function show_hide() {
 
     } else {
         rules.style.display = 'block';
-        btn.innerText = "Close rules";
-        
-    }
+        btn.innerText = "Close rules";   
+    }  
+};
+
+btn.addEventListener('click', show_hide);
+
+
+//Gameboard
+//5x6 game board requiring 30 tiles
+//create board tiles and append to board id in HTML
+
+let box = 30;
+
+for (let i = 0; i < box; i++) { 
+        let tile = document.createElement("input");
+        tile.classList.add("tile");
+        tile.setAttribute("maxlength", 1);
+        document.getElementById("board").appendChild(tile);
     
 };
